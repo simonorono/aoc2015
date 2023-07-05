e468a54c8ebe5809a704a5883d165263126d13b4
Simón Oroño <simonorono@protonmail.com>
2023-07-05 17:01:04 -0400

    Implement method to retrieve the input for a day
    Day now needs the year and the day when initializing it.

--- src/main/kotlin/com/simonorono/advent_of_code/lib/Day.kt
+++ src/main/kotlin/com/simonorono/advent_of_code/lib/Day.kt
@@ -1,6 +1,14 @@
 package com.simonorono.advent_of_code.lib
 
-abstract class Day {
+abstract class Day(private val year: Int, private val day: Int) {
+    fun getInput(): String {
+        return javaClass
+            .classLoader
+            .getResource("input/$year/day$day.txt")!!
+            .readText()
+            .trimEnd()
+    }
+
     abstract fun part1(): String
 
     abstract fun part2(): String
