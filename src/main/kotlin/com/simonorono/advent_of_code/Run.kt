4422fd4662a0a1c32f54c646e7eed0e17c376055
Simón Oroño <simonorono@protonmail.com>
2023-07-05 19:08:34 -0400

    Implement day 1 of year 2022

--- src/main/kotlin/com/simonorono/advent_of_code/Run.kt
+++ src/main/kotlin/com/simonorono/advent_of_code/Run.kt
@@ -2,9 +2,10 @@ package com.simonorono.advent_of_code
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.simonorono.advent_of_code.solutions.y2015.Y2015
+import com.simonorono.advent_of_code.solutions.y2022.Y2022
 
 class Run : CliktCommand(help = "executes the solutions") {
-    private val years = arrayOf(Y2015)
+    private val years = arrayOf(Y2015, Y2022)
 
     override fun run() {
         years.forEach { it.execute() }
